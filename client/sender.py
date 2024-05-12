a61448ef7704956c05d25cfbc9958e2d6a9dde11
dertfin <dertfin@gmail.com>
2024-05-12 13:08:38 +0300

    1.1.7

--- client/sender.py
+++ client/sender.py
@@ -30,22 +30,6 @@ from cryptography.fernet import Fernet
 crypt = get_encryption()    # Получение класса шифрования
 
 
-def send(message: str, session, name):    # Функция отправки сообщения
-    if message == "":
-        print("Вы не можете отправлять пустые сообщения!")
-        return
-    encrypted_message = crypt.encrypt(bytes(message, "utf-8"))    # Шифруем сообщение
-    params = {
-        "session": session,
-        "user": name,
-        "msg": encrypted_message
-    }    # Записываем параетры
-    req = requests.get(f"{server_url}message/new", params = params)    # Посылаем запрос
-
-    if debug: print(f"Отправлено\nЗашафрованное сообщение: {msg}\n")    # Отправка зашифрованного сообщения отправителю
-    return req    # И на всякий статус. Хз зачем, пусть будет, так типо правильно
-
-
 # Получение кол-ва сессий
 server_config = get_server_config()
 
@@ -57,7 +41,7 @@ session = get_session(server_config)    # Номер сессии
 
 # Ход работы
 print("Авторизация...")
-send("Авторизовался", session, name)
+send_message("Авторизовался", session, name, debug=debug)
 while True:
     msg = input(">>> ")
-    send(msg, session, name)
+    send_message(msg, session, name, debug=debug)
